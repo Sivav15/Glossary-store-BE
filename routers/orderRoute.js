3d5f2a3d7ad8acacab30c1842a2d04bc5f4b9f54
sivav15 <sivanathanv1999@gmail.com>
2023-03-02 12:50:37 +0530

    mm

--- routers/orderRoute.js
+++ routers/orderRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { order, yourOrders } = require('../controllers/order');
+const tokenChecker = require('../controllers/tokenCkecker');
 const authentication = require('../middlewares/authentication');
 
 
@@ -10,6 +11,7 @@ const router = express.Router();
 // user
 router.post("/addOrder",authentication,order);
 router.get("/yourOrders/:id",yourOrders);
+router.get("/tokenChecker",authentication,tokenChecker);
 // router.put("/editAddress",editAddress);
 // router.delete("/deleteAddress/:userId/:id",deleteAddress);
 
