c6f11db6e07bd08283398ba173220ff488775643
sivav15 <sivanathanv1999@gmail.com>
2023-03-04 17:57:03 +0530

    gg

--- controllers/auth/sendOtp.js
+++ controllers/auth/sendOtp.js
@@ -38,7 +38,7 @@ try {
 
      saveOtpBackend(email,otp)
     res.status(200).json({
-      message : "Otp send successfull"
+      message : "Otp resend successful"
     })
 } catch (error) {
   console.log(error);
